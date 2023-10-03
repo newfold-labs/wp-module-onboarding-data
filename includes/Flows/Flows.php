8aa3c89d5fde7e9df2d0d2ccd01660a6e548fb8b
Allen Benny <allen.officially@gmail.com>
2023-10-03 13:54:42 +0530

    Update Flows.php

--- includes/Flows/Flows.php
+++ includes/Flows/Flows.php
@@ -16,7 +16,7 @@ final class Flows {
 	 * @var array
 	 */
 	protected static $data = array(
-		'version'              => '1.0.1',
+		'version'              => '1.0.2',
 
 		// Each time step is viewed, insert GMT timestamp to array.
 		'isViewed'             => array(),
