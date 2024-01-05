acff6662448ccafc536e62c0026fb160a5bcb092
arunshenoy99 <devarunshenoy99@gmail.com>
2024-01-05 17:04:09 +0530

    Add the onboarding status option and a coming soon helper

--- includes/Options.php
+++ includes/Options.php
@@ -57,6 +57,7 @@ final class Options {
 		'wc_calc_taxes'                 => 'woocommerce_calc_taxes',
 		'wc_no_sales_tax'               => 'woocommerce_no_sales_tax',
 		'filter_active_plugins'         => 'filter_active_plugins',
+		'status'                        => 'status',
 	);
 
 	/**
@@ -68,7 +69,6 @@ final class Options {
 		'close_comments_for_old_posts'  => 1,
 		'close_comments_days_old'       => 28,
 		'comments_per_page'             => 20,
-		'new_coming_soon'               => 'true',
 		'allow_major_auto_core_updates' => 'true',
 		'allow_minor_auto_core_updates' => 'true',
 		'auto_update_plugin'            => 'true',
