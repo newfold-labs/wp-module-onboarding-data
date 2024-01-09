edf32a4946752ce747027e167acf733baabd8656
lokapure.girish <lokapure.girish@gmail.com>
2024-01-09 17:54:11 +0530

    Update SiteGenData.php

--- includes/SiteGenData.php
+++ includes/SiteGenData.php
@@ -20,7 +20,7 @@ final class SiteGenData {
 			'websiteType' 	=> array(
 				"question" 		=> __( '2. What type of website are you making?', 'wp-module-onboarding' ),
 				"placeholder" 	=> __( 'e.g. Graphic design portfolio', 'wp-module-onboarding' ),
-				"prompt"		=> __( " I am making a website type of", 'wp-module-onboarding' ),
+				"prompt"		=> __( "I am making a website type of", 'wp-module-onboarding' ),
 			),
 			'uniqueBusiness' => array(
 				"question" 		=> __(  "3. Is there anything unique about your business or brand?", 'wp-module-onboarding' ),
