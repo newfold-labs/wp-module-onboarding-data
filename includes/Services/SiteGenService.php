bb600ebdb200f9c3e6ee2cf755ce4eac96cf58e0
Allen Benny <allen.officially@gmail.com>
2024-02-08 19:57:36 +0530

    Update SiteGenService.php

--- includes/Services/SiteGenService.php
+++ includes/Services/SiteGenService.php
@@ -425,9 +425,7 @@ class SiteGenService {
 		$version_number      = $last_version_number + 1;
 
 		foreach ( $homepages as $slug => $data ) {
-			if ( ! preg_match( '/home(\d+)$/', $slug ) ) {
-				continue;
-			}
+			
 			// Select a random palette and check against the parent's palette.
 			$palette_index    = array_rand( $color_palettes );
 			$selected_palette = self::transform_palette( $color_palettes[ $palette_index ], $palette_index );
