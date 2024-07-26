43f35dfdd815b60c029e289ee48ef011932b23b5
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-07-26 17:33:01 +0530

    linting fixes

--- includes/Options.php
+++ includes/Options.php
@@ -57,7 +57,7 @@ final class Options {
 		'filter_active_plugins'         => 'filter_active_plugins',
 		'sitegen_regenerated_homepages' => 'sitegen_regenerated_homepages',
 		'status'                        => 'status',
-		'woocommerce_shop_page_id'		=> 'woocommerce_shop_page_id',
+		'woocommerce_shop_page_id'      => 'woocommerce_shop_page_id',
 	);
 
 	/**
